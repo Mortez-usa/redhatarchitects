ab5eb65af6245f6213c9b72146e74d5ea8defcc7
Mortez-usa <hatam.info@gmail.com>
2025-05-09 17:20:16 -0700

    Fixes navigation links and enables client-side rendering
    Adds 'use client' directive for client-side rendering in the page component.
    Corrects navigation links to use absolute paths for consistency and usability.
    Renames the default export function for better readability.

--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -10,7 +12,8 @@ import {
 
 const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export default function page() {
+export default function Page() {
+
 	return (
 		<div className='flex justify-between items-center px-8 py-4 bg-white'>
 			<header className='w-full p-4 border-b flex items-center justify-between'>
@@ -29,19 +32,19 @@ export default function page() {
 				</Link>
 				<div className='flex items-center gap-6'>
 					<nav className='flex gap-4 text-sm content-center justify-items-center items-center'>
-						<Link href='home' className='hover:underline font-bold'>
+						<Link href='/home' className='hover:underline' >
 							Home
 						</Link>
-						<Link href='browse' className='hover:underline'>
+						<Link href='/browse' className='hover:underline'>
 							Browse
 						</Link>
-						<Link href='requests' className='hover:underline'>
+						<Link href='/requests' className='hover:underline'>
 							Requests
 						</Link>
-						<Link href='pricing' className='hover:underline'>
+						<Link href='/pricing' className='hover:underline'>
 							Pricing
 						</Link>
-						<Link href='about' className='hover:underline'>
+						<Link href='/about' className='hover:underline'>
 							About
 						</Link>
 						<div className='hover:underline'>
